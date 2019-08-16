f394903d3666ae014df7eca58685cc43a5a30aae
PerminovMA <m@pma.su>
2019-08-16 14:17:35 +0300

    test function for AMO realized

--- coda/views.py
+++ coda/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, world! You're at the coda backend.")
 
 
+@csrf_exempt
 def add_new_acc_to_coda(request):
-    return HttpResponse("Post req: " + str(request.POST))
-
+    # return HttpResponse("Post req: " + str(request.POST))
+    return JsonResponse(request.POST)
